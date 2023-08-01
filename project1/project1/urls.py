e2fb07e7b98de0bfee4a068bf2ae64c3a5bf70c7
ali-deeb99 <ali.dib.99.r@gmail.com>
2023-08-01 13:28:35 -0700

    add script1

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('app1.urls')),
+    path('',include('app1.urls'))
     # path('auth/',include('djoser.urls')),
     # path('auth/',include('djoser.urls.authtoken')),
 ]
